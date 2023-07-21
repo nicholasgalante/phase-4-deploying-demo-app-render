3316d34f0534247316b8f9919ed79b1c51b0ec1a
Nicholas Galante <nicholasgalante94@gmail.com>
2023-07-21 10:46:41 -0600

    Update build command

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -2,6 +2,15 @@
 # exit on error
 set -o errexit
 
+# Build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# Build commands for back end
 bundle install
 bundle exec rake db:migrate 
 bundle exec rake db:seed
+
+# Finally, we need to run the following command in the terminal to make sure the script is executable:
+# chmod a+x bin/recipes-build.sh
